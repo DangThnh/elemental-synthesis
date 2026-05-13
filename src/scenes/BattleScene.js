49077376514f780465fbe3bc951f0986e401e178
DangThnh <156942838+DangThnh@users.noreply.github.com>
2026-05-13 15:28:57 +0700

    changes in battle scene file

--- src/scenes/BattleScene.js
+++ src/scenes/BattleScene.js
@@ -830,6 +830,7 @@ export default class BattleScene extends Phaser.Scene {
         }
 
         this.time.delayedCall(2000, () => {
+            this.applyRoundOutcome(finalResult);
             waitScreen.destroy();
             clashText.destroy();
             this.input.enabled = true;
