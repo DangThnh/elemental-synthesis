02081af1c11368e9748c2c449dece54e7007b53d
DangThnh <156942838+DangThnh@users.noreply.github.com>
2026-05-13 15:53:42 +0700

    update

---
+++ src/audio/audioConfig.js
@@ -0,0 +1,16 @@
+/**
+ * Key Phaser cache.audio ↔ đường dẫn trong public/
+ * Thêm file vào thư mục: public/sounds/
+ *   swap.mp3, merge.mp3, crack.mp3, fight.mp3
+ * (đổi tên trong manifest nếu cần)
+ */
+export const AUDIO_MANIFEST = {
+    sfx_swap: 'sounds/swap.mp3',
+    sfx_merge: 'sounds/merge.mp3',
+    sfx_crack: 'sounds/crack.mp3',
+    sfx_fight: 'sounds/fight.mp3',
+    sfx_lose: 'sounds/lose.mp3',
+    sfx_win: 'sounds/win.mp3',
+    sfx_impact: 'sounds/impact.mp3'
+
+};
