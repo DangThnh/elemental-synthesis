af0a865b43710ec50396acbe0c5fb74929129c43
DangThnh <156942838+DangThnh@users.noreply.github.com>
2026-05-21 18:04:57 +0700

    Update

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import Phaser from 'phaser';
+import BattleScene from './scenes/BattleScene';
+// import MapScene from './scenes/MapScene';
+// import PreloaderScene from './scenes/PreloaderScene';
+import './style.css';
+
+// Kích thước chuẩn cho 9:16 (Ví dụ: 720x1280)
+const config = {
+    type: Phaser.AUTO,
+    parent: 'app',
+    width: 720,
+    height: 1280,
+    backgroundColor: '#2d2d2d',
+    scale: {
+        mode: Phaser.Scale.FIT, // Vừa khít màn hình, giữ đúng tỉ lệ 9:16
+        autoCenter: Phaser.Scale.CENTER_BOTH, // Căn giữa màn hình
+    },
+    scene: [BattleScene] 
+};
+
+const game = new Phaser.Game(config);
